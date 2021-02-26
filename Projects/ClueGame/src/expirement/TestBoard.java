504066ee170c88056a33a6896831c30ce29eb8e9
Jeric Wandasan <jtwandasan@mymail.mines.edu>
2021-02-25 19:01:52 -0700

    Driver: Ethan, Navigator: Jeric

---
+++ Projects/ClueGame/src/expirement/TestBoard.java
@@ -0,0 +1,21 @@
+package expirement;
+import java.util.Collections;
+import java.util.Set;
+
+public class TestBoard {
+	
+	public TestBoard() {
+		super();
+	}
+	public void calcTargets(TestBoardCell startCell, int patlength) {
+		
+	}
+	public Set<TestBoardCell> getTargets(){
+		Set<TestBoardCell> testTarget = Collections.emptySet();
+		return testTarget;
+	}
+	public TestBoardCell getCell(int row, int col) {
+		TestBoardCell getCell = new TestBoardCell(row, col);
+		return getCell;
+	}
+}
---
+++ Projects/ClueGame/src/tests/BoardTestsExp.java
@@ -0,0 +1,72 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.*;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import expirement.TestBoard;
+import expirement.TestBoardCell;
+
+class BoardTestsExp {
+	TestBoard board;
+	@BeforeEach 
+	public void setUp(){
+		board = new TestBoard();
+	}
+	@Test
+	public void testAdjacency() {
+		TestBoardCell cell = board.getCell(0, 0);
+		Set<TestBoardCell> testList = cell.getAdjList();
+		Assert.assertTrue(testList.contains(board.getCell(1, 0)));
+		Assert.assertTrue(testList.contains(board.getCell(0, 1)));
+		Assert.assertEquals(2,  testList.size());
+	}
+	@Test
+	public void testEmpty() {
+		TestBoardCell cell = board.getCell(0, 0);
+		Set<TestBoardCell> testList = cell.getAdjList();
+		Assert.assertFalse(testList.isEmpty());
+	}
+	
+	@Test
+	public void testOneOccupied() {
+		TestBoardCell cell = board.getCell(0,0);
+		Set<TestBoardCell> testList = cell.getAdjList();
+		Assert.assertFalse(testList.isEmpty());
+	}
+	@Test
+	public void testOneRoom() {
+		
+	}
+	
+	@Test
+	public void testTargetsNormal() {
+		TestBoardCell cell = board.getCell(0, 0);
+		board.calcTargets(cell, 3);
+		Set<TestBoardCell> targets = board.getTargets();
+		Assert.assertEquals(6, targets.size());
+		Assert.assertTrue(targets.contains(board.getCell(3, 0)));
+		Assert.assertTrue(targets.contains(board.getCell(2, 1)));
+		Assert.assertTrue(targets.contains(board.getCell(0, 1)));
+		Assert.assertTrue(targets.contains(board.getCell(1, 2)));
+		Assert.assertTrue(targets.contains(board.getCell(0, 3)));
+		Assert.assertTrue(targets.contains(board.getCell(1, 0)));
+	}		
+		@Test
+	public void testTargetsMixed() {
+		board.getCell(0, 2).setOccupied(true);
+		board.getCell(0,2).setIsRoom(true);
+		TestBoardCell cell = board.getCell(0, 3);
+		board.calcTargets(cell, 3);
+		Set<TestBoardCell> targets = board.getTargets();
+		Assert.assertEquals(3, targets.size());
+		Assert.assertTrue(targets.contains(board.getCell(1, 2)));
+		Assert.assertTrue(targets.contains(board.getCell(2, 2)));
+		Assert.assertTrue(targets.contains(board.getCell(3, 3)));
+	}
+	
+}
